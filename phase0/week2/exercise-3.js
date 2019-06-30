f51cbc0ce7900fee79feedff7573f2970729ac05
andreassosilo <andreas.sosilo@gmail.com>
2019-06-30 20:41:22 +0700

    Revisi exercise-3.js

--- phase0/week2/exercise-3.js
+++ phase0/week2/exercise-3.js
@@ -6,24 +6,24 @@ let nama = "Andreas";
 let peran = "Penyihir";
 
 // if-else conditional
-if (nama == "" && peran == "") {
+if (nama === "" && peran === "") {
     console.log("Nama harus diisi!");
 }
-else if (typeof nama == "string" && peran == "") {
+else if (typeof nama === "string" && peran === "") {
     console.log("Halo " + nama + ". Pilih peranmu untuk memulai game!");
 }
-else if (typeof nama == "string" && peran == "Ksatria") {
+else if (typeof nama === "string" && peran === "Ksatria") {
     console.log("Selamat datang di Dunia Proxytia, " + nama);
     console.log("Halo Ksatria " + nama + ", kamu dapat menyerang dengan senjatamu!");
 }
-else if (typeof nama == "string" && peran == "Tabib") {
+else if (typeof nama === "string" && peran === "Tabib") {
     console.log("Selamat datang di Dunia Proxytia, " + nama);
     console.log("Halo Tabib " + nama + ", kamu akan membantu temanmu yang terluka.");
 }
-else if (typeof nama == "string" && peran == "Penyihir") {
+else if (typeof nama === "string" && peran === "Penyihir") {
     console.log("Selamat datang di Dunia Proxytia, " + nama);
     console.log("Halo Penyihir " + nama + ", ciptakan keajaiban yang membantu kemenanganmu!");
 }
 else {
-    console.log("NaN");
+    console.log("Salah input!");
 }
