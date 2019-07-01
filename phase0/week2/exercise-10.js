090fe33af7ad3ac891604d44dea04d0cc9f0fe01
andreassosilo <andreas.sosilo@gmail.com>
2019-07-01 09:32:04 +0700

    Add exercise-10.js

---
+++ phase0/week2/exercise-10.js
@@ -0,0 +1,23 @@
+// Exercise 10 - Compare Numbers
+// Andreas Sosilo - Hacktiv8 Batch 34 - Humble Fox
+
+// Comparing 2 numbers
+function bandingkanAngka(angka1, angka2) {
+    if (angka2 > angka1) {
+        return true;
+    }
+    else if (angka2 < angka1) {
+        return false;
+
+    }
+    else {
+        return -1;
+    }
+}
+  
+// TEST CASES
+console.log(bandingkanAngka(5, 8)); // true
+console.log(bandingkanAngka(5, 3)); // false
+console.log(bandingkanAngka(4, 4)); // -1
+console.log(bandingkanAngka(3, 3)); // -1
+console.log(bandingkanAngka(17, 2)); // false
