622851d31ff2a2955a00f6e26bcea244b5294fe8
andreassosilo <andreas.sosilo@gmail.com>
2019-07-01 12:35:41 +0700

    Revised exercise-12.js

--- phase0/week2/exercise-12.js
+++ phase0/week2/exercise-12.js
@@ -5,17 +5,19 @@
 function konversiMenit(menit) {
     let hour;
     let minute;
+    let formattedMinute;
     let newMinutesDisplay;
 
     hour = Math.floor(menit/60);
     minute = menit%60;
-    newMinutesDisplay = hour + ':' + minute;
+    formattedMinute = ("0" + minute).slice(-2);
+    newMinutesDisplay = hour + ':' + formattedMinute;
     return newMinutesDisplay;
-  }
-  
-  // TEST CASES
-  console.log(konversiMenit(63)); // 1:03
-  console.log(konversiMenit(124)); // 2:04
-  console.log(konversiMenit(53)); // 0:53
-  console.log(konversiMenit(88)); // 1:28
-  console.log(konversiMenit(120)); // 2:00
+}
+
+// TEST CASES
+console.log(konversiMenit(63)); // 1:03
+console.log(konversiMenit(124)); // 2:04
+console.log(konversiMenit(53)); // 0:53
+console.log(konversiMenit(88)); // 1:28
+console.log(konversiMenit(120)); // 2:00
