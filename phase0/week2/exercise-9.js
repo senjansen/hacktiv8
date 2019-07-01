f0ee34101b2275fe2ad997f626d8ab469f83a7a3
andreassosilo <andreas.sosilo@gmail.com>
2019-07-01 07:39:15 +0700

    Add exercise-9.js

---
+++ phase0/week2/exercise-9.js
@@ -0,0 +1,34 @@
+// Exercise 9 - Function
+// Andreas Sosilo - Hacktiv8 Batch 34 - Humble Fox
+
+// No.1 : Function shoutOut()
+function shoutOut() {
+    return 'Halo Function!';
+}
+
+console.log(shoutOut()) // Menampilkan "Halo Function!" di console
+
+// No.2 : Function calculateMultiply(num1, num2)
+function calculateMultiply(num1, num2) {
+    return num1*num2;
+}
+
+var num1 = 5;
+var num2 = 6;
+
+var hasilPerkalian = calculateMultiply(num1,num2);
+console.log(hasilPerkalian); // Menampilkan angka 30
+
+// No.3 : Function processSentence
+function processSentence(name, age, address, hobby) {
+    let sentence = 'Nama saya ' + name + ', umur saya ' + age + ', alamat saya di ' + address + ', dan saya punya hobby yaitu ' + hobby + '!';
+    return sentence;
+} 
+
+ var name = "Agus";
+ var age = 30;
+ var address = "Jln. Malioboro, Yogjakarta";
+ var hobby = "gaming";
+ 
+ var fullSentence = processSentence(name,age,address,hobby);
+ console.log(fullSentence); // Menampilkan "Nama saya Agus, umur saya 30 tahun, alamat saya di Jln. Malioboro, Yogjakarta, dan saya punya hobby yaitu gaming!"
