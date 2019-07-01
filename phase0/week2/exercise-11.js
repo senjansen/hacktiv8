f0cbb138890032a07165314a54bb1e4b8bd7b0ae
andreassosilo <andreas.sosilo@gmail.com>
2019-07-01 10:19:06 +0700

    Revised exercise-11.js

--- phase0/week2/exercise-11.js
+++ phase0/week2/exercise-11.js
@@ -10,11 +10,11 @@ function balikKata(kata) {
         reverseWord += kata[wordLength-counter];
     }
     return reverseWord;
-  }
-  
-  // TEST CASES
-  console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
-  console.log(balikKata('John Doe')); // eoD nhoJ
-  console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
-  console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
-  console.log(balikKata('Super')); // repuS
+}
+
+// TEST CASES
+console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(balikKata('John Doe')); // eoD nhoJ
+console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(balikKata('Super')); // repuS
