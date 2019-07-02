be6fe35da4ee2afde2a0c62c3748d3189c769cd6
andreassosilo <andreas.sosilo@gmail.com>
2019-07-02 15:41:18 +0700

    Modify exercise-4.js week3

--- phase0/week3/exercise-4.js
+++ phase0/week3/exercise-4.js
@@ -3,12 +3,79 @@
 
 // Create function to display data of multidimensional array
 function dataHandling2(input) {
-    for (let counterRow = 0; counterRow < input.length; counterRow++) {
-        console.log(`Nomor ID: ${input[counterRow][0]}`);
-        console.log(`Nama Lengkap: ${input[counterRow][1]}`);
-        console.log(`TTL: ${input[counterRow][2]} ${input[counterRow][3]}`);
-        console.log(`Hobi: ${input[counterRow][4]}\n`);
+    let tanggal;
+    // Using splice() method
+    input.splice(1, 1, 'Roman Alamsyah Elsharawy');
+    input.splice(2, 1, 'Provinsi Bandar Lampung');
+    input.splice(4, 1, 'Pria', 'SMA Internasional Metro');
+    console.log(input);
+
+    // Using split() method
+    tanggal = input[3].split('/');
+    console.log(tanggal);
+
+    // switch-case
+    switch(tanggal[1]) {
+        case '01':
+            console.log('Januari');
+            break;
+
+        case '02':       
+            console.log('Februari');
+            break;
+
+        case '03':
+            console.log('Maret');
+            break;
+
+        case '04':
+            console.log('April');
+            break;
+
+        case '05':
+            console.log('Mei');
+            break;
+
+        case '06':
+            console.log('Juni');
+            break;
+
+        case '07':
+            console.log('Juli');
+            break;
+
+        case '08':
+            console.log('Agustus');
+            break;
+        
+        case '09':
+            console.log('September');
+            break;
+
+        case '10':
+            console.log('Oktober');
+            break;
+
+        case '11':
+            console.log('November');
+            break;
+
+        case '12':
+            console.log('Desember');
+            break;
+
+        default:
+            console.log('Salah bulan!');
+            break;
     }
+
+    // sorting tanggal
+    console.log(tanggal.sort());
+    arr.sort();
+}
+
+function(value1, value2) { 
+    return value1 > value2 
 }
 
 var input = ["0001", "Roman Alamsyah ", "Bandar Lampung", "21/05/1989", "Membaca"];
