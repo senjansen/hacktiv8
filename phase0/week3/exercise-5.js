74a3df2e4d73b32e05ae5fcd6106350249133550
andreassosilo <andreas.sosilo@gmail.com>
2019-07-02 21:32:12 +0700

    Adding eslintrc.js and package.json

---
+++ phase0/week3/exercise-5.js
@@ -0,0 +1,15 @@
+/* eslint-disable semi */
+// Exercise 5 - Palindrom
+// Andreas Sosilo - Hacktiv8 Batch 34 - Humble Fox
+
+// Create function to return true or false based on palindrom
+function palindrome (kata) {
+  // you can only write your code here!
+}
+
+// TEST CASES
+console.log(palindrome('katak')); // true
+console.log(palindrome('blanket')); // false
+console.log(palindrome('civic')); // true
+console.log(palindrome('kasur rusak')); // true
+console.log(palindrome('mister')); // false
