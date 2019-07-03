fcf18378d24d30432a5fa5dee363b897decf31b0
andreassosilo <andreas.sosilo@gmail.com>
2019-07-03 07:17:33 +0700

    Adding exercise-7.js for week3

---
+++ phase0/week3/exercise-7.js
@@ -0,0 +1,14 @@
+// Exercise 7 - Counting Words
+// Andreas Sosilo - Hacktiv8 Batch 34 - Humble Fox
+
+// Create function to count number of words
+function hitungJumlahKata (kalimat) {
+  // you can only write your code here!
+}
+
+// TEST CASES
+console.log(hitungJumlahKata('I have a dream')) // 4
+console.log(hitungJumlahKata('Never eat shredded wheat or cake')) // 6
+console.log(hitungJumlahKata('A song to sing')) // 4
+console.log(hitungJumlahKata('I')) // 1
+console.log(hitungJumlahKata('I believe I can code')) // 5
