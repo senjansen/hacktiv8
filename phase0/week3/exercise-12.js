870a26ed6596e5d663ee7ff7862f7b2a66735234
andreassosilo <andreas.sosilo@gmail.com>
2019-07-03 18:26:34 +0700

    Add exercise-12.js week3

---
+++ phase0/week3/exercise-12.js
@@ -0,0 +1,28 @@
+// Exercise 12 - Geometry Progression
+// Andreas Sosilo - Hacktiv8 Batch 34 - Humble Fox
+
+// Create function to check the geometry series of an array number
+function tentukanDeretGeometri (arr) {
+  let multiplier = 0
+  let checker = false
+
+  multiplier = arr[1] / arr[0]
+
+  for (let counter = 1; counter < arr.length - 1; counter++) {
+    if (arr[counter + 1] / arr[counter] === multiplier) {
+      checker = true
+    } else {
+      checker = false
+    }
+    // console.log('Counter: ' + counter + ' = ' + checker);
+  }
+
+  return checker
+}
+
+// TEST CASES
+console.log(tentukanDeretGeometri([1, 3, 9, 27, 81])) // true
+console.log(tentukanDeretGeometri([2, 4, 8, 16, 32])) // true
+console.log(tentukanDeretGeometri([2, 4, 6, 8])) // false
+console.log(tentukanDeretGeometri([2, 6, 18, 54])) // true
+console.log(tentukanDeretGeometri([1, 2, 3, 4, 7, 9])) // false
