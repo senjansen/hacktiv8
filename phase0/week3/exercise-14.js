7931abfa7c9764cef84fa4ba9ab409a726e7467c
andreassosilo <andreas.sosilo@gmail.com>
2019-07-04 14:10:29 +0700

    Add exercise-14.js week3

---
+++ phase0/week3/exercise-14.js
@@ -0,0 +1,13 @@
+// Exercise 14 - Grouping Numbers
+// Andreas Sosilo - Hacktiv8 Batch 34 - Humble Fox
+
+// Create function to group numbers in a multidimensional array
+function mengelompokkanAngka (arr) {
+  // you can only write your code here!
+}
+
+// TEST CASES
+console.log(mengelompokkanAngka([2, 4, 6])) // [ [2, 4], [], [6] ]
+console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])) // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
+console.log(mengelompokkanAngka([100, 151, 122, 99, 111])) // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
+console.log(mengelompokkanAngka([])) // [ [], [], [] ]
