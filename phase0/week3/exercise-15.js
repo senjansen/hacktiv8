3f3d128d7d87ca9b47291b4b9cd304c6ee94414e
andreassosilo <andreas.sosilo@gmail.com>
2019-07-04 16:31:35 +0700

    Modify exercise-15.js week3

---
+++ phase0/week3/exercise-15.js
@@ -0,0 +1,33 @@
+// Exercise 15 - Grouping Numbers
+// Andreas Sosilo - Hacktiv8 Batch 34 - Humble Fox
+
+// Create function to group numbers in a multidimensional array
+function groupAnimals (animals) {
+  let groupingArray = []
+  let tempArray = []
+
+  // Use sort() method to sort the animals array first
+  animals = animals.sort()
+
+  // Grouping animals according to alphabet using forEach() method
+  animals.forEach(function (element) {
+    let name = element.toUpperCase() // to ignore upper and lower case
+    let id = name.charAt(0)
+    if (!(tempArray[id] && tempArray[id].length)) {
+      tempArray[id] = []
+    }
+    tempArray[id].push(element)
+  })
+
+  // Using for-in statement to push the grouped animals to new array
+  for (let counter in tempArray) {
+    groupingArray.push(tempArray[counter])
+  }
+  return groupingArray
+}
+
+// TEST CASES
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']))
+// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak']))
+// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
