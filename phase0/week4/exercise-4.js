4707aaad1d7650caef6f9a38366ee94d4c54e5bf
andreassosilo <andreas.sosilo@gmail.com>
2019-07-05 12:59:30 +0700

    Add exercise-4.js week4

---
+++ phase0/week4/exercise-4.js
@@ -0,0 +1,14 @@
+// Exercise 3 - Looking for Median
+// Andreas Sosilo - Hacktiv8 Batch 34 - Humble Fox
+
+// Create function to check the median from an array
+function cariModus (arr) {
+  // you can only write your code here!
+}
+
+// TEST CASES
+console.log(cariModus([10, 4, 5, 2, 4])) // 4
+console.log(cariModus([5, 10, 10, 6, 5])) // 5
+console.log(cariModus([10, 3, 1, 2, 5])) // -1
+console.log(cariModus([1, 2, 3, 3, 4, 5])) // 3
+console.log(cariModus([7, 7, 7, 7, 7])) // -1
