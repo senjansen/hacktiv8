99d637b0aeceec511a1081ce7c13faeeec88417b
andreassosilo <andreas.sosilo@gmail.com>
2019-07-07 16:49:23 +0700

    Modify exercise-5.js week2 using JavaScript Standard Style

--- phase0/week2/exercise-5.js
+++ phase0/week2/exercise-5.js
@@ -2,60 +2,60 @@
 // Andreas Sosilo - Hacktiv8 Batch 34 - Humble Fox
 
 // No.1 : Let's Form A Sentence
-var word = 'JavaScript';
-var second = 'is';
-var third = 'awesome';
-var fourth = 'and';
-var fifth = 'I';
-var sixth = 'love';
-var seventh = 'it!';
+var word = 'JavaScript'
+var second = 'is'
+var third = 'awesome'
+var fourth = 'and'
+var fifth = 'I'
+var sixth = 'love'
+var seventh = 'it!'
 
-console.log(word + ' ' + second + ' ' + third + ' ' + fourth + ' ' + fifth + ' ' + sixth + ' ' + seventh);
+console.log(word + ' ' + second + ' ' + third + ' ' + fourth + ' ' + fifth + ' ' + sixth + ' ' + seventh)
 
 // No.2 : Index Accessing - 1 by 1
-var word = 'wow JavaScript is so cool';
-var exampleFirstWord = word[0] + word[1] + word[2];
-var secondWord = word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13]; 
-var thirdWord = word[15] + word[16]; 
-var fourthWord = word[18] + word[19]; 
-var fifthWord = word[21] + word[22] + word[23] + word[24];
+word = 'wow JavaScript is so cool'
+var exampleFirstWord = word[0] + word[1] + word[2]
+var secondWord = word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13]
+var thirdWord = word[15] + word[16]
+var fourthWord = word[18] + word[19]
+var fifthWord = word[21] + word[22] + word[23] + word[24]
 
-console.log('\nFirst Word: ' + exampleFirstWord);
-console.log('Second Word: ' + secondWord);
-console.log('Third Word: ' + thirdWord);
-console.log('Fourth Word: ' + fourthWord);
-console.log('Fifth Word: ' + fifthWord);
+console.log('\nFirst Word: ' + exampleFirstWord)
+console.log('Second Word: ' + secondWord)
+console.log('Third Word: ' + thirdWord)
+console.log('Fourth Word: ' + fourthWord)
+console.log('Fifth Word: ' + fifthWord)
 
 // No.3 : Breaking Sentence using SubString
-var word3 = 'wow JavaScript is so cool';
-var exampleFirstWord3 = word3.substring(0, 3);
-var secondWord3 = word3.substring(4, 14); 
-var thirdWord3 = word3.substring(15, 17);
-var fourthWord3 = word3.substring(18, 20); 
-var fifthWord3 = word3.substring(21, 25); 
+var word3 = 'wow JavaScript is so cool'
+var exampleFirstWord3 = word3.substring(0, 3)
+var secondWord3 = word3.substring(4, 14)
+var thirdWord3 = word3.substring(15, 17)
+var fourthWord3 = word3.substring(18, 20)
+var fifthWord3 = word3.substring(21, 25)
 
-console.log('\nFirst Word: ' + exampleFirstWord3);
-console.log('Second Word: ' + secondWord3);
-console.log('Third Word: ' + thirdWord3);
-console.log('Fourth Word: ' + fourthWord3);
-console.log('Fifth Word: ' + fifthWord3);
+console.log('\nFirst Word: ' + exampleFirstWord3)
+console.log('Second Word: ' + secondWord3)
+console.log('Third Word: ' + thirdWord3)
+console.log('Fourth Word: ' + fourthWord3)
+console.log('Fifth Word: ' + fifthWord3)
 
 // No.4 : Breaking Sentence and Count Each Lenght
-var word4 = 'wow JavaScript is so cool';
-var exampleFirstWord4 = word4.substring(0, 3);
-var secondWord4 = word4.substring(4, 14); 
-var thirdWord4 = word4.substring(15, 17);
-var fourthWord4 = word4.substring(18, 20); 
-var fifthWord4 = word4.substring(21, 25); 
+var word4 = 'wow JavaScript is so cool'
+var exampleFirstWord4 = word4.substring(0, 3)
+var secondWord4 = word4.substring(4, 14)
+var thirdWord4 = word4.substring(15, 17)
+var fourthWord4 = word4.substring(18, 20)
+var fifthWord4 = word4.substring(21, 25)
 
-var firstWordLength = exampleFirstWord4.length;
-var secondWordLength = secondWord4.length;
-var thirdWordLength = thirdWord4.length;
-var fourthWordLength = fourthWord4.length;
-var fifthWordLength = fifthWord4.length;
+var firstWordLength = exampleFirstWord4.length
+var secondWordLength = secondWord4.length
+var thirdWordLength = thirdWord4.length
+var fourthWordLength = fourthWord4.length
+var fifthWordLength = fifthWord4.length
 
-console.log('\nFirst Word: ' + exampleFirstWord + ', with length: ' + firstWordLength);
-console.log('Second Word: ' + secondWord4 + ', with length: ' + secondWordLength);
-console.log('Third Word: ' + thirdWord4 + ', with length: ' + thirdWordLength);
-console.log('Fourth Word: ' + fourthWord4 + ', with length: ' + fourthWordLength);
-console.log('Fifth Word: ' + fifthWord4 + ', with length: ' + fifthWordLength);
+console.log('\nFirst Word: ' + exampleFirstWord + ', with length: ' + firstWordLength)
+console.log('Second Word: ' + secondWord4 + ', with length: ' + secondWordLength)
+console.log('Third Word: ' + thirdWord4 + ', with length: ' + thirdWordLength)
+console.log('Fourth Word: ' + fourthWord4 + ', with length: ' + fourthWordLength)
+console.log('Fifth Word: ' + fifthWord4 + ', with length: ' + fifthWordLength)
