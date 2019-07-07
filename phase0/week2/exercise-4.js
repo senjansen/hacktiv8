8e2c1290e5856e9348c18b8f117257fc052b4fe4
andreassosilo <andreas.sosilo@gmail.com>
2019-07-07 16:44:45 +0700

    Modify exercise-4.js using JavaScript Standard Style

--- phase0/week2/exercise-4.js
+++ phase0/week2/exercise-4.js
@@ -2,60 +2,60 @@
 // Andreas Sosilo - Hacktiv8 Batch 34 - Humble Fox
 
 // Declare variables
-let hari = 14; // between (1-31)
-let bulan = 5; // between (1-12)
-let tahun = 2012; // between (1900-2200)
+let hari = 14 // between (1-31)
+let bulan = 5 // between (1-12)
+let tahun = 2012 // between (1900-2200)
 
 // switch-case conditional
 switch (bulan) {
-    case 1:
-        console.log(hari + " Januari " + tahun);
-        break;
+  case 1:
+    console.log(hari + ' Januari ' + tahun)
+    break
 
-    case 2:
-        console.log(hari + " Februari " + tahun);
-        break;
-            
-    case 3:
-        console.log(hari + " Maret " + tahun);
-        break;
+  case 2:
+    console.log(hari + ' Februari ' + tahun)
+    break
 
-    case 4:
-        console.log(hari + " April " + tahun);
-        break;
+  case 3:
+    console.log(hari + ' Maret ' + tahun)
+    break
 
-    case 5:
-        console.log(hari + " Mei " + tahun);
-        break;
+  case 4:
+    console.log(hari + ' April ' + tahun)
+    break
 
-    case 6:
-        console.log(hari + " Juni " + tahun);
-        break;
+  case 5:
+    console.log(hari + ' Mei ' + tahun)
+    break
 
-    case 7:
-        console.log(hari + " Juli " + tahun);
-        break;
+  case 6:
+    console.log(hari + ' Juni ' + tahun)
+    break
 
-    case 8:
-        console.log(hari + " Agustus " + tahun);
-        break;
+  case 7:
+    console.log(hari + ' Juli ' + tahun)
+    break
 
-    case 9:
-        console.log(hari + " September " + tahun);
-        break;
+  case 8:
+    console.log(hari + ' Agustus ' + tahun)
+    break
 
-    case 10:
-        console.log(hari + " Oktober " + tahun);
-        break;
+  case 9:
+    console.log(hari + ' September ' + tahun)
+    break
 
-    case 11:
-        console.log(hari + " November " + tahun);
-        break;
+  case 10:
+    console.log(hari + ' Oktober ' + tahun)
+    break
 
-    case 12:
-        console.log(hari + " Desember " + tahun);
-        break;
+  case 11:
+    console.log(hari + ' November ' + tahun)
+    break
 
-    default:
-        console.log("Bulan yang dimasukkan salah!");
+  case 12:
+    console.log(hari + ' Desember ' + tahun)
+    break
+
+  default:
+    console.log('Bulan yang dimasukkan salah!')
 }
