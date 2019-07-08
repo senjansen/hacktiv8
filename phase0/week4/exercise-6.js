d95c9c7d4698af0b7824128d618fcff9223889b4
andreassosilo <andreas.sosilo@gmail.com>
2019-07-08 14:57:23 +0700

    Add exercise-6.js week4

---
+++ phase0/week4/exercise-6.js
@@ -0,0 +1,14 @@
+// Exercise 6 - Minimum Digit of Multiplication
+// Andreas Sosilo - Hacktiv8 Batch 34 - Humble Fox
+
+// Create function to return the minimin digit of multiplication from a number
+function digitPerkalianMinimum (angka) {
+
+}
+
+// TEST CASES
+console.log(digitPerkalianMinimum(24)) // 2
+console.log(digitPerkalianMinimum(90)) // 3
+console.log(digitPerkalianMinimum(20)) // 2
+console.log(digitPerkalianMinimum(179)) // 4
+console.log(digitPerkalianMinimum(1)) // 2
