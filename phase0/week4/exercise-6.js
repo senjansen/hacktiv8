e2f8b8f8b35eaf2734a5d4f7289561c47308edf1
andreassosilo <andreas.sosilo@gmail.com>
2019-07-08 20:38:48 +0700

    Modify exercise-6.js week4

--- phase0/week4/exercise-6.js
+++ phase0/week4/exercise-6.js
@@ -3,9 +3,29 @@
 
 // Create function to return the minimum digit of multiplication from a number
 function digitPerkalianMinimum (angka) {
-  let temp = 0
+  let resultArr = []
+  let result = ''
 
-    
+  for (let num1 = 1; num1 <= angka; num1++) {
+    let combine = ''
+    if (angka % num1 === 0) {
+      let num2 = angka / num1
+      combine = combine + num1 + num2
+      // console.log('Combine:' + combine)
+      resultArr.push(combine)
+    }
+  }
+
+  for (let counter = 0; counter < resultArr.length; counter++) {
+    if (result === '') {
+      result = resultArr[counter].length
+    }
+    if (result > resultArr[counter].length) {
+      result = resultArr[counter].length
+    }
+    // console.log('Result:' + result)
+  }
+  return result
 }
 
 // TEST CASES
