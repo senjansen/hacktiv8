5c89853fcfd141cd2a48a8a9bc2aaf1e2eabb0da
andreassosilo <andreas.sosilo@gmail.com>
2019-07-09 22:19:00 +0700

    Add exercise-15.js week4

---
+++ phase0/week4/exercise-15.js
@@ -0,0 +1,86 @@
+// Exercise 15 - Highest Score
+// Andreas Sosilo - Hacktiv8 Batch 34 - Humble Fox
+
+// Create function to display the best student with the highest score from each class
+function highestScore (students) {
+  let bestStudent = {}
+
+  for (let i = 0; i < students.length; i++) {
+    if (bestStudent[students[i].class] === undefined) {
+      bestStudent[students[i].class] = { name: students[i].name, score: students[i].score }
+      // console.log(bestStudent)
+    } else {
+      if (bestStudent[students[i].class].score < students[i].score) {
+        bestStudent[students[i].class].name = students[i].name
+        bestStudent[students[i].class].score = students[i].score
+      }
+      // console.log(bestStudent)
+    }
+  }
+  return bestStudent
+}
+
+// TEST CASE
+console.log(highestScore([
+  {
+    name: 'Dimitri',
+    score: 90,
+    class: 'foxes'
+  },
+  {
+    name: 'Alexei',
+    score: 85,
+    class: 'wolves'
+  },
+  {
+    name: 'Sergei',
+    score: 74,
+    class: 'foxes'
+  },
+  {
+    name: 'Anastasia',
+    score: 78,
+    class: 'wolves'
+  }
+]))
+
+// {
+//   foxes: { name: 'Dimitri', score: 90 },
+//   wolves: { name: 'Alexei', score: 85 }
+// }
+
+console.log(highestScore([
+  {
+    name: 'Alexander',
+    score: 100,
+    class: 'foxes'
+  },
+  {
+    name: 'Alisa',
+    score: 76,
+    class: 'wolves'
+  },
+  {
+    name: 'Vladimir',
+    score: 92,
+    class: 'foxes'
+  },
+  {
+    name: 'Albert',
+    score: 71,
+    class: 'wolves'
+  },
+  {
+    name: 'Viktor',
+    score: 80,
+    class: 'tigers'
+  }
+]))
+
+// {
+//   foxes: { name: 'Alexander', score: 100 },
+//   wolves: { name: 'Alisa', score: 76 },
+//   tigers: { name: 'Viktor', score: 80 }
+// }
+
+console.log(highestScore([])) // {}
