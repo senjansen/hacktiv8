0d9cac1ad38dfce8eb3318ff6fb80536eb5b97f7
andreassosilo <andreas.sosilo@gmail.com>
2019-07-09 22:33:55 +0700

    Add exercise-16.js week4

---
+++ phase0/week4/exercise-16.js
@@ -0,0 +1,94 @@
+// Exercise 16 - Graduates
+// Andreas Sosilo - Hacktiv8 Batch 34 - Humble Fox
+
+// Create function to display the students who graduate with score > 75 from each class
+function graduates (students) {
+  let graduateList = {}
+
+  for (let i = 0; i < students.length; i++) {
+    if (students[i].score > 75) {
+      if (graduateList[students[i].class] === undefined) {
+        graduateList[students[i].class] = [{ name: students[i].name, score: students[i].score }]
+      } else {
+        graduateList[students[i].class].push({ name: students[i].name, score: students[i].score })
+      }
+    }
+  }
+  return graduateList
+}
+
+console.log(graduates([
+  {
+    name: 'Dimitri',
+    score: 90,
+    class: 'foxes'
+  },
+  {
+    name: 'Alexei',
+    score: 85,
+    class: 'wolves'
+  },
+  {
+    name: 'Sergei',
+    score: 74,
+    class: 'foxes'
+  },
+  {
+    name: 'Anastasia',
+    score: 78,
+    class: 'wolves'
+  }
+]))
+
+// {
+//   foxes: [
+//     { name: 'Dimitri', score: 90 }
+//   ],
+//   wolves: [
+//     { name: 'Alexei' , score: 85 },
+//     { name: 'Anastasia', score: 78 }
+//   ]
+// }
+
+console.log(graduates([
+  {
+    name: 'Alexander',
+    score: 100,
+    class: 'foxes'
+  },
+  {
+    name: 'Alisa',
+    score: 76,
+    class: 'wolves'
+  },
+  {
+    name: 'Vladimir',
+    score: 92,
+    class: 'foxes'
+  },
+  {
+    name: 'Albert',
+    score: 71,
+    class: 'wolves'
+  },
+  {
+    name: 'Viktor',
+    score: 80,
+    class: 'tigers'
+  }
+]))
+
+// {
+//   foxes: [
+//     { name: 'Alexander', score: 100 },
+//     { name: 'Vladimir', score: 92 }
+//   ],
+//   wolves: [
+//     { name: 'Alisa', score: 76 },
+//   ],
+//   tigers: [
+//     { name: 'Viktor', score: 80 }
+//   ]
+// }
+
+console.log(graduates([])) // {}
