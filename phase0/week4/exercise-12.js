588ad6d74d5dfe560189c651f6e2a8c00f54401f
andreassosilo <andreas.sosilo@gmail.com>
2019-07-09 17:40:35 +0700

    Modify exercise-12.js week4

--- phase0/week4/exercise-12.js
+++ phase0/week4/exercise-12.js
@@ -48,7 +48,6 @@ console.log(countProfit([{ name: 'Windi', product: 'Sepatu Stacattu', amount: 2 
 //   shoppers: [],
 //   leftOver: 1,
 //   totalProfit: 0 } ]
-
 console.log(countProfit([{ name: 'Windi', product: 'Sepatu Stacattu', amount: 8 }, { name: 'Vanessa', product: 'Sepatu Stacattu', amount: 10 }, { name: 'Rani', product: 'Sweater Uniklooh', amount: 1 }, { name: 'Devi', product: 'Baju Zoro', amount: 1 }, { name: 'Lisa', product: 'Baju Zoro', amount: 1 }]))
 // [ { product: 'Sepatu Stacattu',
 //     shoppers: [ 'Windi' ],
