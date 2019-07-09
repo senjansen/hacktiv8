4c4ff8210ce21aa22b8f2cdebd8ccb720db660aa
andreassosilo <andreas.sosilo@gmail.com>
2019-07-09 07:57:15 +0700

    Add exercise-10 week4

---
+++ phase0/week4/exercise-10.js
@@ -0,0 +1,20 @@
+// Exercise 10 - Change Me!
+// Andreas Sosilo - Hacktiv8 Batch 34 - Humble Fox
+
+// Create function to show object literal that has firstName, lastName, gender and age properties
+function changeMe (arr) {
+  // you can only write your code here!
+}
+
+// TEST CASES
+changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]) // 1. Christ Evans:
+// { firstName: 'Christ',
+//   lastName: 'Evans',
+//   gender: 'Male',
+//   age: 37 }
+// 2. Robert Downey:
+// { firstName: 'Robert',
+//   lastName: 'Downey',
+//   gender: 'Male',
+//   age: 'Invalid Birth Year' }
+changeMe([]) // ""
