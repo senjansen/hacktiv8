a4b57c81e12408fe1ebb08f5ee3b56372aef29bc
andreassosilo <andreas.sosilo@gmail.com>
2019-07-10 16:07:32 +0700

    Add reduce.js

---
+++ phase0/week5/reduce.js
@@ -0,0 +1,12 @@
+let arr = [[1, 2], [2, 3], [3, 4]];
+
+console.log(
+  arr.reduce(function(sum, b) {
+    return (
+      sum +
+      b.reduce(function(c, d) {
+        return c + d;
+      }, 0)
+    );
+  }, 0)
+);
