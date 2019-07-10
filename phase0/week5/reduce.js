41fd0118264917b9965ab268abffe8a3feedee16
andreassosilo <andreas.sosilo@gmail.com>
2019-07-10 16:58:53 +0700

    Add eslinttrc.js in week5 folder

--- phase0/week5/reduce.js
+++ phase0/week5/reduce.js
@@ -1,12 +1,19 @@
-let arr = [[1, 2], [2, 3], [3, 4]];
+let arr = [[1, 2], [2, 3], [3, 4]]
 
 console.log(
-  arr.reduce(function(sum, b) {
+  arr.reduce(function (sum, b) {
     return (
       sum +
-      b.reduce(function(c, d) {
-        return c + d;
+      b.reduce(function (c, d) {
+        return c + d
       }, 0)
-    );
+    )
   }, 0)
-);
+)
+
+let a = 0
+let b = 2
+
+for (let i = 2; i < b; i++) {
+  b += a
+}
