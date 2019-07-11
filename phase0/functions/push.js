4ecd8431f38b51765381c56b054047f5932aa46e
andreassosilo <andreas.sosilo@gmail.com>
2019-07-11 20:19:17 +0700

    Modify push.js

--- phase0/functions/push.js
+++ phase0/functions/push.js
@@ -1,10 +1,10 @@
 // Manual push() => The push() method adds one or more elements to the end of an array and returns the new length of the array.
 
 function push (arr, ...rest) {
-  var indexOfArr = arr.length - 1
+  let indexOfArr = arr.length - 1
 
-  // do loop, to add rest value to array
-  for (var i = 0; i < rest.length; i++) {
+  // Do loop, to add rest value to array
+  for (let i = 0; i < rest.length; i++) {
     indexOfArr++
     arr[indexOfArr] = rest[i]
   }
@@ -12,6 +12,5 @@ function push (arr, ...rest) {
   return arr
 }
 
-var array = [0, 1, 2, 3, 4, 5]
-
-console.log(push(array, 'tambah1', 'tambah2'))
+let array = [0, 1, 2, 3, 4, 5]
+console.log(push(array, 'tambah1', 'tambah2')) // [0, 1, 2, 3, 4, 5, 'tambah1', 'tambah2']
