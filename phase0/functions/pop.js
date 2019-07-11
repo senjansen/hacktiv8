dacee8402fe801bfb7952abfd82648362f60066a
andreassosilo <andreas.sosilo@gmail.com>
2019-07-11 20:02:12 +0700

    Modify pop.js

--- phase0/functions/pop.js
+++ phase0/functions/pop.js
@@ -4,15 +4,17 @@ function pop (arr) {
   let indexOfTempArr = 0
   let tempArr = []
 
-  // do loop. to create new array without last data of array
+  // Do loop. to create new array without last data of array
   for (let i = 0; i < array.length - 1; i++) {
     tempArr[indexOfTempArr] = arr[i]
     indexOfTempArr++
   }
-
-  return tempArr
+  arr = tempArr
+  return arr
 }
 
 let array = [0, 1, 2, 3, 4]
-
-console.log(pop(array))
+array = pop(array)
+console.log(array) // [0, 1, 2, 3]
+array = pop(array)
+console.log(array) // [0, 1, 2]
