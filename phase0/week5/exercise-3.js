60f523cc1677d5ee34a9fa1941ee30e8c54e1385
andreassosilo <andreas.sosilo@gmail.com>
2019-07-11 09:53:14 +0700

    Add exercise-3.js week5

---
+++ phase0/week5/exercise-3.js
@@ -0,0 +1,18 @@
+// Exercise 2 - All You Can Eat (Recursion)
+// Andreas Sosilo - Hacktiv8 Batch 34 - Humble Fox
+
+// Create function to reduce 15 minutes for every dishes that was eaten, given a certain time input
+function makanTerusRekursif (waktu) {
+  if (waktu > 0) {
+    return 1 + makanTerusRekursif(waktu - 15)
+  } else {
+    return 0
+  }
+}
+
+// TEST CASES
+console.log(makanTerusRekursif(66)) // 5
+console.log(makanTerusRekursif(100)) // 7
+console.log(makanTerusRekursif(90)) // 6
+console.log(makanTerusRekursif(10)) // 1
+console.log(makanTerusRekursif(0)) // 0
