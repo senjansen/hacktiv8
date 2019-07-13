eb1c32e0a38fb3ec6f7d67fb80a74a761e8cfd13
andreassosilo <andreas.sosilo@gmail.com>
2019-07-14 05:47:49 +0700

    Modify exercise-5.js using standard JS

--- phase0/week3/exercise-5.js
+++ phase0/week3/exercise-5.js
@@ -1,19 +1,18 @@
-/* eslint-disable semi */
 // Exercise 5 - Palindrom
 // Andreas Sosilo - Hacktiv8 Batch 34 - Humble Fox
 
 // Create function to return true or false based on palindrom
 function palindrome (kata) {
   // reverse the input using several array built-in functions
-  let reverseString = kata.split('').reverse().join('');
-  console.log(kata);
-  console.log(reverseString);
-  return reverseString === kata;
+  let reverseString = kata.split('').reverse().join('')
+  console.log(kata)
+  console.log(reverseString)
+  return reverseString === kata
 }
 
 // TEST CASES
-console.log(palindrome('katak')); // true
-console.log(palindrome('blanket')); // false
-console.log(palindrome('civic')); // true
-console.log(palindrome('kasur rusak')); // true
-console.log(palindrome('mister')); // false
+console.log(palindrome('katak')) // true
+console.log(palindrome('blanket')) // false
+console.log(palindrome('civic')) // true
+console.log(palindrome('kasur rusak')) // true
+console.log(palindrome('mister')) // false
