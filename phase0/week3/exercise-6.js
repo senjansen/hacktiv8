940e66345c2eb13d06ce2c5252ca2e1dc6185b28
andreassosilo <andreas.sosilo@gmail.com>
2019-07-14 06:12:50 +0700

    Modify exercise-6.js week3 using standard JS

--- phase0/week3/exercise-6.js
+++ phase0/week3/exercise-6.js
@@ -1,24 +1,24 @@
-/* eslint-disable semi */
 // Exercise 6 - Palindrom Number
 // Andreas Sosilo - Hacktiv8 Batch 34 - Humble Fox
 
 // Create function to return the next palindrom number
 function angkaPalindrome (num) {
+  // Find the next number that is palindrome
+  num++
   // reverse the input using several array built-in functions
-  num++;
-  let reverseNum = num.toString().split('').reverse().join('');
+  let reverseNum = num.toString().split('').reverse().join('')
 
   if (parseInt(reverseNum) === num) {
-    return num;
+    return num
   } else {
     // console.log('bukan palindrom:' + num);
-    return angkaPalindrome(num);
+    return angkaPalindrome(num)
   }
 }
 
 // TEST CASES
-console.log(angkaPalindrome(8)); // 9
-console.log(angkaPalindrome(10)); // 11
-console.log(angkaPalindrome(117)); // 121
-console.log(angkaPalindrome(175)); // 181
-console.log(angkaPalindrome(1000)); // 1001
+console.log(angkaPalindrome(8)) // 9
+console.log(angkaPalindrome(10)) // 11
+console.log(angkaPalindrome(117)) // 121
+console.log(angkaPalindrome(175)) // 181
+console.log(angkaPalindrome(1000)) // 1001
