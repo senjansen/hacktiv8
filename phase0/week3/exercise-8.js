d1ee4e316c79989f6368a6a8cab5307cd628dabc
andreassosilo <andreas.sosilo@gmail.com>
2019-07-14 06:30:55 +0700

    Modify exercise-8.js week3

--- phase0/week3/exercise-8.js
+++ phase0/week3/exercise-8.js
@@ -25,4 +25,3 @@ console.log(pasanganTerbesar(12783456)) // 83
 console.log(pasanganTerbesar(910233)) // 91
 console.log(pasanganTerbesar(71856421)) // 85
 console.log(pasanganTerbesar(79918293)) // 99
-console.log(4134)
