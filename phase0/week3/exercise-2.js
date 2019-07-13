c453caa1b8c4b122389a099c19c04798b23d9c65
andreassosilo <andreas.sosilo@gmail.com>
2019-07-14 05:17:41 +0700

    Modify exercise-2.js with standard JS

--- phase0/week3/exercise-2.js
+++ phase0/week3/exercise-2.js
@@ -2,21 +2,21 @@
 // Andreas Sosilo - Hacktiv8 Batch 34 - Humble Fox
 
 // Create function to reverse value in array
-function balikString(arrayInput) {
-    let arrayLength = arrayInput.length;
-    let reverseWord = "";
+function balikString (arrayInput) {
+  let arrayLength = arrayInput.length
+  let reverseWord = ''
 
-    for (let counter = 1; counter <= arrayLength; counter++) {
-        reverseWord += arrayInput[arrayLength-counter];
-    }
-    return reverseWord;
+  for (let counter = 1; counter <= arrayLength; counter++) {
+    reverseWord += arrayInput[arrayLength - counter]
+  }
+  return reverseWord
 }
 
 // Declare variable
-let arrayInput = "hello world!";
+let arrayInput = 'hello world!'
 
 // Print reverse string
-console.log(balikString(arrayInput));
+console.log(balikString(arrayInput))
 
-//input "hello world!"
-//output "!dlrow olleh"
+// input "hello world!"
+// output "!dlrow olleh"
