ca615449df0415715273b3f72a41469fc89c28cc
andreassosilo <andreas.sosilo@gmail.com>
2019-07-14 05:33:18 +0700

    Modify exercise-4.js using standard JS

--- phase0/week3/exercise-4.js
+++ phase0/week3/exercise-4.js
@@ -2,89 +2,88 @@
 // Andreas Sosilo - Hacktiv8 Batch 34 - Humble Fox
 
 // Create function to display data of multidimensional array
-function dataHandling2(input) {
-    let tanggal;
-    let tanggalSort;
-    let tanggalJoin;
-    // Using splice() method
-    input.splice(1, 1, 'Roman Alamsyah Elsharawy');
-    input.splice(2, 1, 'Provinsi Bandar Lampung');
-    input.splice(4, 1, 'Pria', 'SMA Internasional Metro');
-    console.log(input);
+function dataHandling2 (input) {
+  let tanggal
+  let tanggalSort
+  let tanggalJoin
+  // Using splice() method
+  input.splice(1, 1, 'Roman Alamsyah Elsharawy')
+  input.splice(2, 1, 'Provinsi Bandar Lampung')
+  input.splice(4, 1, 'Pria', 'SMA Internasional Metro')
+  console.log(input)
 
-    // Using split() method
-    tanggal = input[3].split('/');
+  // Using split() method
+  tanggal = input[3].split('/')
 
-    // switch-case
-    switch(tanggal[1]) {
-        case '01':
-            console.log('Januari');
-            break;
+  // switch-case
+  switch (tanggal[1]) {
+    case '01':
+      console.log('Januari')
+      break
 
-        case '02':       
-            console.log('Februari');
-            break;
+    case '02':
+      console.log('Februari')
+      break
 
-        case '03':
-            console.log('Maret');
-            break;
+    case '03':
+      console.log('Maret')
+      break
 
-        case '04':
-            console.log('April');
-            break;
+    case '04':
+      console.log('April')
+      break
 
-        case '05':
-            console.log('Mei');
-            break;
+    case '05':
+      console.log('Mei')
+      break
 
-        case '06':
-            console.log('Juni');
-            break;
+    case '06':
+      console.log('Juni')
+      break
 
-        case '07':
-            console.log('Juli');
-            break;
+    case '07':
+      console.log('Juli')
+      break
 
-        case '08':
-            console.log('Agustus');
-            break;
-        
-        case '09':
-            console.log('September');
-            break;
+    case '08':
+      console.log('Agustus')
+      break
 
-        case '10':
-            console.log('Oktober');
-            break;
+    case '09':
+      console.log('September')
+      break
 
-        case '11':
-            console.log('November');
-            break;
+    case '10':
+      console.log('Oktober')
+      break
 
-        case '12':
-            console.log('Desember');
-            break;
+    case '11':
+      console.log('November')
+      break
 
-        default:
-            console.log('Salah bulan!');
-            break;
-    }
+    case '12':
+      console.log('Desember')
+      break
 
-    // Using sort() method --> sorting tanggal
-    tanggalJoin = tanggal.join('-');
-    tanggalSort = tanggal.sort(function(value1, value2){return value2-value1});
-    console.log(tanggalSort);
+    default:
+      console.log('Salah bulan!')
+      break
+  }
 
-    // Using join() method
-    console.log(tanggalJoin);
+  // Using sort() method --> sorting tanggal
+  tanggalJoin = tanggal.join('-')
+  tanggalSort = tanggal.sort(function (value1, value2) { return value2 - value1 })
+  console.log(tanggalSort)
 
-    // Using slice() method
-    console.log(input[1].slice(0,15));
+  // Using join() method
+  console.log(tanggalJoin)
 
+  // Using slice() method
+  console.log(input[1].slice(0, 15))
 }
 
-var input = ["0001", "Roman Alamsyah ", "Bandar Lampung", "21/05/1989", "Membaca"];
-dataHandling2(input);
+var input = ['0001', 'Roman Alamsyah ', 'Bandar Lampung', '21/05/1989', 'Membaca']
+dataHandling2(input)
 
 /**
  * keluaran yang diharapkan (pada console)
