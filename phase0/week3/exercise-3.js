1ff31f54e77b5f1f4bf4c7fdd30973562aaf9f5d
andreassosilo <andreas.sosilo@gmail.com>
2019-07-14 05:22:44 +0700

    Modify exercise-3.js with standard JS

--- phase0/week3/exercise-3.js
+++ phase0/week3/exercise-3.js
@@ -2,21 +2,21 @@
 // Andreas Sosilo - Hacktiv8 Batch 34 - Humble Fox
 
 // Create function to display data of multidimensional array
-function dataHandling(input) {
-    for (let counterRow = 0; counterRow < input.length; counterRow++) {
-        console.log(`Nomor ID: ${input[counterRow][0]}`);
-        console.log(`Nama Lengkap: ${input[counterRow][1]}`);
-        console.log(`TTL: ${input[counterRow][2]} ${input[counterRow][3]}`);
-        console.log(`Hobi: ${input[counterRow][4]}\n`);
-    }
+function dataHandling (input) {
+  for (let i = 0; i < input.length; i++) {
+    console.log(`Nomor ID: ${input[i][0]}`)
+    console.log(`Nama Lengkap: ${input[i][1]}`)
+    console.log(`TTL: ${input[i][2]} ${input[i][3]}`)
+    console.log(`Hobi: ${input[i][4]}\n`)
+  }
 }
 
-//contoh input
+// contoh input
 var input = [
-    ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
-    ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
-    ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
-    ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
-];
+  ['0001', 'Roman Alamsyah', 'Bandar Lampung', '21/05/1989', 'Membaca'],
+  ['0002', 'Dika Sembiring', 'Medan', '10/10/1992', 'Bermain Gitar'],
+  ['0003', 'Winona', 'Ambon', '25/12/1965', 'Memasak'],
+  ['0004', 'Bintang Senjaya', 'Martapura', '6/4/1970', 'Berkebun']
+]
 
-dataHandling(input);
+dataHandling(input)
