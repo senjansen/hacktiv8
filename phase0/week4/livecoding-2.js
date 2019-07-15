117a24f334683e498018cf2d7ce491184235257b
andreassosilo <andreas.sosilo@gmail.com>
2019-07-15 22:49:29 +0700

    Add livecoding-2.js week4

---
+++ phase0/week4/livecoding-2.js
@@ -0,0 +1,104 @@
+// Live Coding Week 4 - Question 1
+// Andreas Sosilo - Hacktiv8 Batch 34 - Humble Fox
+
+/*
+  /////////////////
+  americanGotTalent
+  /////////////////
+  Function americanGotTalent akan menentukan juara serta jumlah votingnya dari suatu array.
+  Function akan menerima 1 parameter yaitu:
+    - votes adalah kumpulan string nama, tipe datanya array.
+  Function ini akan mengembalikkan nilai string dalam format:
+    - Apabila terdapat juara: "Selamat juaranya adalah [nama], dengan total votes yang diterima [totalVotes]".
+    - Apabila tidak terdapat juara: "Mohon maaf juara tidak bisa ditentukkan dalam minggu ini".
+
+  ----------
+  EXAMPLE 1:
+  ----------
+  [INPUT]
+    var votes = ['Lee Min Ho', 'Arby', 'Darren McFlurry', 'Lee Min Ho'];
+  [PROCESS]
+    terdapat 2 voting Lee Min Ho yang paling banyak.
+  [OUTPUT]
+    Selamat juaranya adalah Lee Min Ho, dengan total votes yang diterima 2.
+  ----------
+  EXAMPLE 2:
+  ----------
+  [INPUT]
+    var votes = ['Lee Min Ho', 'Arby', 'Arby', 'Lee Min Ho'];
+  [PROCESS]
+    tidak terdapat yang terbanyak.
+  [OUTPUT]
+    Mohon maaf juara tidak bisa ditentukkan dalam minggu ini
+
+  [RULES]
+    1. Dilarang menggunakan built in function apapun kecuali .push(), .pop(), .shift() dan .unshift()
+*/
+
+function americanGotTalent (votes) {
+  let totalVotes = []
+  let name = []
+  let counter = 0
+  let mostName = ''
+  let mostVotes = 0
+  let status = false
+
+  // Sort input
+  votes = sorting(votes)
+  // console.log(votes)
+  // Define the first
+  name.push(votes[0])
+  name.push(1)
+  totalVotes.push(name)
+
+  for (let i = 1; i < votes.length; i++) {
+    if (votes[i] === votes[i - 1]) {
+      totalVotes[counter][1] += 1
+    } else {
+      name = []
+      name.push(votes[i])
+      name.push(1)
+      totalVotes.push(name)
+      counter++
+    }
+    // console.log(`i: ${i} and totalVotes: ${totalVotes}`)
+  }
+
+  for (let i = 0; i < totalVotes.length; i++) {
+    if (totalVotes[i][1] > mostVotes) {
+      mostVotes = totalVotes[i][1]
+      mostName = totalVotes[i][0]
+      status = true
+    } else if (totalVotes[i][1] === mostVotes) {
+      status = false
+    }
+  }
+
+  if (status) {
+    return `Selamat juaranya ${mostName}, dengan total votes yang diterima ${mostVotes}`
+  } else {
+    return 'Mohon maaf juara tidak bisa ditentukkan dalam minggu ini'
+  }
+}
+
+function sorting (arr) {
+  let arrLength = arr.length
+
+  for (let i = 0; i < arrLength; i++) {
+    for (let j = 0; j < arrLength; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+      }
+    }
+  }
+  return arr
+}
+
+console.log(americanGotTalent(['Lee Min Ho', 'Arby', 'Darren McFlurry', 'Lee Min Ho']))
+// Selamat juaranya adalah Lee Min Ho, dengan total votes yang diterima 2
+console.log(americanGotTalent(['Lee Min Ho', 'Arby', 'Arby', 'Lee Min Ho']))
+// Mohon maaf juara tidak bisa ditentukkan dalam minggu ini
+console.log(americanGotTalent(['Lee Min Ho', 'Arby', 'Arby']))
+// Selamat juaranya adalah Arby, dengan total votes yang diterima 2
