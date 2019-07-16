fdf01ba1ad0ef4785a4a1bc25fde64d088724b84
andreassosilo <andreas.sosilo@gmail.com>
2019-07-16 20:42:07 +0700

    Add exercises files

---
+++ phase0/week3/Practice Sort/review2.js
@@ -0,0 +1,8 @@
+// TEST LATIHAN 1
+
+function manualSort() {
+  
+}
+
+console.log(manualSort([3, 4, 2, 1, 5, 6, 7]));
+// [1, 2, 3, 4, 5, 6, 7]
---
+++ phase0/week3/Practice Sort/review3.js
@@ -0,0 +1,13 @@
+// TEST LATIHAN 2
+
+function manualSort() {
+  
+}
+
+console.log(manualSort([[3, 4, 2], [5, 1, 6, 7]]));
+/*
+  [
+    [2, 3, 4],
+    [1, 5, 6, 7]
+  ]
+*/
