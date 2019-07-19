d73549f167ab7f95d72716b6596d73d2d736ad79
andreassosilo <andreas.sosilo@gmail.com>
2019-07-19 21:39:16 +0700

    Refactoring exercise-7.js week4

--- phase0/week4/exercise-7.js
+++ phase0/week4/exercise-7.js
@@ -3,8 +3,21 @@
 
 // Create function to sort alphabet in a string
 function urutkanAbjad (str) {
-  let sortedString = str.split('')
-  return sortedString.sort().join('')
+  // Split the string to array
+  let sortStr = str.split('')
+  // Compare the first item with the second. If the first item is bigger than the
+  // second item. you swap them so that the bigger one stays in the second position.
+  for (let i = sortStr.length; i >= 0; i--) {
+    for (let j = 1; j <= i; j++) {
+      if (sortStr[j - 1] > sortStr[j]) {
+        let temp = sortStr[j - 1]
+        sortStr[j - 1] = sortStr[j]
+        sortStr[j] = temp
+      }
+    }
+  }
+  // Join the array into a string
+  return sortStr.join('')
 }
 
 // TEST CASES
@@ -13,3 +26,9 @@ console.log(urutkanAbjad('truncate')) // 'acenrttu'
 console.log(urutkanAbjad('developer')) // 'deeeloprv'
 console.log(urutkanAbjad('software')) // 'aeforstw'
 console.log(urutkanAbjad('aegis')) // 'aegis'
+
+// With refactoring the code
+/* function urutkanAbjad (str) {
+  let sortedString = str.split('')
+  return sortedString.sort().join('')
+} */
