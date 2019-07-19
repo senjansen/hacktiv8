cf4710ef2d4e0987ade79dc84f165b35c1310c01
andreassosilo <andreas.sosilo@gmail.com>
2019-07-19 14:20:46 +0700

    Modify reviewWeek4.js

--- phase0/week4/lectureArrayOfObject/reviewWeek4.js
+++ phase0/week4/lectureArrayOfObject/reviewWeek4.js
@@ -1,7 +1,10 @@
 /*
 Outline yang wajib dikuasai student:
 1. Mampu membuat object
-2.
+2. Mampu mengubah tabel menjadi bentukkan array of object
+3. Mampu mengakses object termasuk object di dalam array
+4. Mampu melooping dan memanipulasi object
+5. Mampu melooping dan memanipulasi array of object
 */
 
 // Membuat array & object
