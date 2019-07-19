bf975963b5edb10216c5003ecb609fe3c156b07d
andreassosilo <andreas.sosilo@gmail.com>
2019-07-19 21:23:48 +0700

    Refactoring exercise-6.js week4

--- phase0/week4/exercise-6.js
+++ phase0/week4/exercise-6.js
@@ -5,25 +5,23 @@
 function digitPerkalianMinimum (angka) {
   let resultArr = []
   let result = ''
-
-  for (let num1 = 1; num1 <= angka; num1++) {
+  // Check only until square root of the number
+  for (let num1 = 1; num1 <= Math.sqrt(angka); num1++) {
     let combine = ''
+    // Find all of the factor from the number and push to the array
     if (angka % num1 === 0) {
       let num2 = angka / num1
       combine = combine + num1 + num2
-      console.log('Combine:' + combine)
+      // console.log('Combine:' + combine)
       resultArr.push(combine)
     }
   }
 
   for (let counter = 0; counter < resultArr.length; counter++) {
-    if (result === '') {
+    if (!result || result > resultArr[counter].length) {
       result = resultArr[counter].length
     }
-    if (result > resultArr[counter].length) {
-      result = resultArr[counter].length
-    }
-    console.log('Result:' + result)
+    // console.log('Result:' + result)
   }
   return result
 }
