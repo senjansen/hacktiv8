76769d5510fb76132e9ff27c8cf5b9cc0561a356
andreassosilo <andreas.sosilo@gmail.com>
2019-07-19 11:53:39 +0700

    Add reviewWeek4.js

---
+++ phase0/week4/lectureArrayOfObject/reviewWeek4.js
@@ -0,0 +1,18 @@
+/*
+Outline yang wajib dikuasai student:
+1. Mampu membuat object
+2.
+*/
+
+// Membuat array & object
+let array = []
+let object = {
+  'instructor name': 'Andreas', // key-value pairs
+  address: 'Jakarta'
+}
+
+console.log(object)
+
+console.log(object.address)
+let key = 'instructor name'
+console.log(object[key])
