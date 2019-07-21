66072b14e6a0eb7b593add894323acfbcecb64b5
andreassosilo <andreas.sosilo@gmail.com>
2019-07-21 14:59:12 +0700

    Modify objectLiteral.js

--- phase0/week4/lectureArrayOfObject/objectLiteral.js
+++ phase0/week4/lectureArrayOfObject/objectLiteral.js
@@ -12,13 +12,19 @@
   3. Cara membuat array menjadi unik
 */
 
-// 1. membuat object literal (3 times)
+// 1. Membuat object literal (3 times)
 
 function createObjectLiteral (name, batch, hobbies) {
-
+  let studentObj = {
+    name,
+    batch,
+    hobbies
+  }
+  return studentObj
 }
 
-// console.log(createObjectLiteral('', 0, []))
+// console.log(createObjectLiteral('Andreas', 34, ['Reading', 'Table Tennis']))
+
 /*
   {
     name: '....',
@@ -27,13 +33,19 @@ function createObjectLiteral (name, batch, hobbies) {
   }
 */
 
-// 2. mengakses object literal (3 times)
+// 2. Mengakses object literal (3 times)
 
-var objectLiteral = {
-  name: '',
-  batch: 0,
-  hobbies: []
+let objectLiteral = {
+  name: 'Andreas',
+  batch: 34,
+  hobbies: ['Reading', 'Table Tennis']
 }
+
+function printObjectValue (person) {
+  return `Nama saya adalah ${person.name} \nSaya berada di batch ${person.batch} \nHobby saya adalah ${person.hobbies}`
+}
+
+// console.log(printObjectValue(objectLiteral))
 
 /*
   // Nama saya adalah .....
@@ -41,7 +53,7 @@ var objectLiteral = {
   // Hobby saya adalah ....
 */
 
-// 3. melooping object (7 times)
+// 3. Melooping object (7 times)
 // object berbeda dengan array, karena object tidak punya index
 // jadi urutan di object harusnya ngga dipermasalahkan, sehingga
 // apabila ingin melakukan operasi yang harus berurutan lebih baik
@@ -50,17 +62,34 @@ var objectLiteral = {
 // notes: apabila properties dari object merupakan number, dia akan secara
 // otomatis akan berurutan. sehingga bisa dimanfaatkan untuk kebutuhan tertentu
 
-var hogwarts = {
+let hogwarts = {
   gryffindor: 30,
   hufflepuff: 29,
   ravenclaw: 29,
   slytherin: 28
 }
 
-// 4. mengubah dari array ke object (5 times)
+function printKeyValue (hogwarts) {
+  let string = ''
+  let key = Object.keys(hogwarts)
+  for (let i = 0; i < key.length; i++) {
+    string = string + key[i] + ': ' + hogwarts[key[i]] + '\n'
+  }
+  return string
+}
+
+// console.log(printKeyValue(hogwarts))
+
+// 4. Mengubah dari array ke object (5 times)
 
 function changeArrayToObject (array) {
-
+  let resultObj = {}
+  for (let i = 0; i < array.length; i++) {
+    if (!resultObj[array[i][0]] || array[i][1] > resultObj[array[i][0]]) {
+      resultObj[array[i][0]] = array[i][1]
+    }
+  }
+  return resultObj
 }
 
 // 4.1 ini satu semua
@@ -83,3 +112,18 @@ function changeArrayToObject (array) {
 
 // 5. bonus Object.keys dan Object.values
 // ubah hogwarts ke dalam bentuk array properties/keysnya dan array bentuk valuesnya
+
+function objectToArray (obj) {
+  let array = []
+  let keys = Object.keys(obj)
+  let values = Object.values(obj)
+  for (let i = 0; i < keys.length; i++) {
+    let arrayTemp = []
+    arrayTemp.push(keys[i])
+    arrayTemp.push(values[i])
+    array.push(arrayTemp.slice())
+  }
+  return array
+}
+
+console.log(objectToArray(hogwarts))
