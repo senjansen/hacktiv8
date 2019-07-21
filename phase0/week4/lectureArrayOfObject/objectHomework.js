9a4e2a3964e22acc46fe7093e8feda8653f12589
andreassosilo <andreas.sosilo@gmail.com>
2019-07-21 16:31:47 +0700

    Modify objectHomework.js

--- phase0/week4/lectureArrayOfObject/objectHomework.js
+++ phase0/week4/lectureArrayOfObject/objectHomework.js
@@ -7,22 +7,56 @@
   }
 */
 
+function createObject (person) {
+  return {
+    name: person[0],
+    batch: person[1],
+    hobbies: person[2]
+  }
+}
+
+// console.log(createObject(['Andreas', 'Batch 34 - Humble Fox', ['Reading', 'Cycling']]))
+
 // 2. Memasukkan array ke dalam object
 // Urutannya [NAME, CLUB, AGE]
-var insertToObjects = [
+let insertToObjects = [
   ['Kapten Tsubasa', 'Nankatsu', 19],
   ['Wakabayashi', 'Nankatsu', 20],
   ['Saitama', 'Hero Associate', 18]
 ]
 
-// 3. Membuat array menjadi unik
-
-function makeUnique(arrayOfNames) {
-
+function arrayToObject (array) {
+  let obj = {}
+  for (let i = 0; i < array.length; i++) {
+    obj['PLAYER' + (i + 1)] = {
+      NAME: array[i][0],
+      CLUB: array[i][1],
+      AGE: array[i][2]
+    }
+  }
+  return obj
 }
 
-// console.log(makeUnique(
-//   ['Lee Min Ho', 'Lee Kwang Ho', 'Lee Min Ho', 'Lindan']
-// ))
+console.log(arrayToObject(insertToObjects))
 
+// 3. Membuat array menjadi unik
+
+function makeUnique (arrayOfNames) {
+  let result = []
+  result.push(arrayOfNames[0])
+  for (let i = 1; i < arrayOfNames.length; i++) {
+    let isUnique = true
+    for (let j = 0; j < result.length; j++) {
+      if (arrayOfNames[i] === result[j]) {
+        isUnique = false
+      }
+    }
+    if (isUnique) {
+      result.push(arrayOfNames[i])
+    }
+  }
+  return result
+}
+
+console.log(makeUnique(['Lee Min Ho', 'Lee Kwang Ho', 'Lee Min Ho', 'Lindan']))
 // ['Lee Min Ho', 'Lee Kwang Ho', 'Lindan']
